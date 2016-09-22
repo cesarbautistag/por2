3dd99224f3cb0ef14f9d9d5b39f60a87e84ca26d
Cesar Bautista Garcia <cebaga@floridauniversitaria.es>
2016-09-22 10:06:51 +0200

    paco guapo

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//fichero index.php
+//creado por Paco Gomez
+?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+        echo "César";
+       ?>
+       <br>
+      <?php
+        echo "Bautista";
+        ?>
+        <br>
+      <?php
+        echo "García";
+        ?>
+        <br>
+      
+  </head>
+  <body>
+</html>
