cbb2ba42aebf21e61d6e4c91da026e75c86a57f6
Cesar Bautista Garcia <cebaga@floridauniversitaria.es>
2016-10-20 10:23:02 +0200

    hiiii

---
+++ while/por2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tabla del 2</title>
+  </head>
+  <body>
+    <?php
+    $numero=1;
+
+    while ($numero <= 10) {
+      $resultado=$numero*2;
+      echo "2 por $numero = $resultado <br>";
+      $numero++;
+    }?>
+
+  </body>
+</html>
